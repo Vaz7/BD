5a27e059ae3160fea703885b57c3cbba5b2f6bab
Gonçalo Marinho <a90969@alunos.uminho.pt>
2023-06-03 20:53:20 +0100

    update nas permissoes

--- src/Triggers.sql
+++ src/Triggers.sql
@@ -1,13 +1,13 @@
 USE videomino
 
-
+-- ESTE TRIGGER ATUALIZA O STOCK DOS FILMES QUE SÃO COMPRADOS
 
 DELIMITER $$
-CREATE TRIGGER atualizarStock AFTER INSERT ON Compra_filme
+CREATE TRIGGER atualizarStockCompra AFTER INSERT ON Compra_filme
 FOR EACH ROW
 BEGIN
     IF NEW.quantidade > 0 AND EXISTS (
-        SELECT 1 FROM Compra
+        SELECT 1 FROM Compra -- isto faz uma tabela de 1's, só para saber se há algum
         WHERE idCompra = NEW.id_compra
         AND data_prevista = CURDATE()
     ) THEN
@@ -19,14 +19,14 @@ END $$
 DELIMITER ;
 
 
-
+-- ESTE TRIGGER ATUALIZAR O STOCK DOS FILMES QUE SÃO VENDIDOS
 
 DELIMITER $$
 CREATE TRIGGER atualizarStockVenda AFTER INSERT ON Venda_filme
 FOR EACH ROW
 BEGIN
     IF NEW.quantidade > 0 AND EXISTS (
-        SELECT 1 FROM Venda
+        SELECT 1 FROM Venda 
         WHERE idVenda = NEW.id_Venda
         AND data = CURDATE()
     ) THEN
--- src/users.sql
+++ src/users.sql
@@ -10,6 +10,9 @@ GRANT SELECT ON Videomino.Cliente TO 'funcionario'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON Videomino.Filme TO 'funcionario'@'localhost';
 GRANT SELECT ON Videomino.Review TO 'funcionario'@'localhost';
 GRANT SELECT ON Videomino.Compra_filme TO 'funcionario'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.ClientesTable TO 'funcionario'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.VerificaFilmeStock TO 'funcionario'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.ComprasFornecedores TO 'funcionario'@'localhost';
 
 -- CLIENTE 
 -- DROP USER 'cliente'@'localhost';
@@ -17,6 +20,11 @@ CREATE USER 'cliente'@'localhost' IDENTIFIED BY 'cliente';
 GRANT SELECT ON Videomino.Filme TO 'cliente'@'localhost';
 GRANT SELECT ON Videomino.Review TO 'cliente'@'localhost';
 GRANT INSERT, UPDATE ON Videomino.Cliente TO 'cliente'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.FilmesTable TO 'cliente'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.FilmesEntrePreços TO 'cliente'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.FilmeGenero TO 'cliente'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.OrdenaFilmesData TO 'cliente'@'localhost';
+GRANT EXECUTE ON PROCEDURE Videomino.OrdenaFilmesRating TO 'cliente'@'localhost';
 
 
 CREATE USER 'fornecedor'@'localhost' IDENTIFIED BY 'fornecedor';
