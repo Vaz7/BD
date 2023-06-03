559ffebf9db17befb64f896ec5b78ef72958f85d
Gonçalo Marinho <a90969@alunos.uminho.pt>
2023-06-03 18:11:28 +0100

    Functions.sql

--- src/users.sql
+++ src/users.sql
@@ -1,13 +1,18 @@
+-- ADMINISTRADOR
+-- DROP USER 'admin'@'localhost';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON Videomino.* TO 'admin'@'localhost';
 
-
+-- FUNCIONARIO
+-- DROP USER 'funcionario'@'localhost';
 CREATE USER 'funcionario'@'localhost' IDENTIFIED BY 'funcionario';
 GRANT SELECT ON Videomino.Cliente TO 'funcionario'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON Videomino.Filme TO 'funcionario'@'localhost';
 GRANT SELECT ON Videomino.Review TO 'funcionario'@'localhost';
 GRANT SELECT ON Videomino.Compra_filme TO 'funcionario'@'localhost';
 
+-- CLIENTE 
+-- DROP USER 'cliente'@'localhost';
 CREATE USER 'cliente'@'localhost' IDENTIFIED BY 'cliente';
 GRANT SELECT ON Videomino.Filme TO 'cliente'@'localhost';
 GRANT SELECT ON Videomino.Review TO 'cliente'@'localhost';
@@ -15,4 +20,6 @@ GRANT INSERT, UPDATE ON Videomino.Cliente TO 'cliente'@'localhost';
 
 
 CREATE USER 'fornecedor'@'localhost' IDENTIFIED BY 'fornecedor';
+
+
 FLUSH PRIVILEGES;
